672d8aece0cbd56f900a666ecf16c8588652ed62
sungah <letsspeak01@naver.com>
2026-01-11 20:10:12 +0900

    Fix: Vercel serverless function handler 수정
    - Express 앱이 요청을 제대로 처리하도록 handler 함수 추가
    - 파일 내용이 그대로 반환되던 문제 해결

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,9 @@
 // Vercel serverless function entry point
-// Express 앱을 serverless function으로 래핑
+// 모든 요청을 Express 앱으로 라우팅
 const app = require('../server.js');
 
 // Vercel serverless function handler
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // Express 앱이 요청을 처리하도록 전달
   return app(req, res);
 };
