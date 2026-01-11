28ef1f04f9df47dd4a151015b88a248979779642
sungah <letsspeak01@naver.com>
2026-01-11 20:11:04 +0900

    Fix: Vercel Express app export 방식 수정
    - Express 앱을 직접 export하여 Vercel이 자동으로 handler로 인식하도록 변경
    - 파일 내용이 그대로 반환되던 문제 해결

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 // Vercel serverless function entry point
 // 모든 요청을 Express 앱으로 라우팅
-const app = require('../server.js');
+const path = require('path');
+
+// server.js의 경로를 절대 경로로 설정
+const serverPath = path.join(__dirname, '..', 'server.js');
+const app = require(serverPath);
 
 // Vercel serverless function handler
-module.exports = async (req, res) => {
-  // Express 앱이 요청을 처리하도록 전달
-  return app(req, res);
-};
+// Express 앱을 직접 export하면 Vercel이 자동으로 handler로 인식
+module.exports = app;
 
