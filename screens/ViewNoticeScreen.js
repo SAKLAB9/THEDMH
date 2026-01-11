217b6af7976cc595a14b0785da50b959ae8a48d8
sungah <letsspeak01@naver.com>
2026-01-11 21:14:51 +0900

    Fix: useRef import 추가

--- screens/ViewNoticeScreen.js
+++ screens/ViewNoticeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { View, Text, ScrollView, Image, ActivityIndicator, Alert, TouchableOpacity, Dimensions, TextInput, Modal, Linking } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
